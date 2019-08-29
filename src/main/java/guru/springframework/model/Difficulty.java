c0bc051c36109e2502dafafafe168ae985e714ee
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2019-08-29 16:55:42 -0500

    Added Repositories for Category, Recipe, and UnitOfMeasure. Fixed Entity bug

--- src/main/java/guru/springframework/model/Difficulty.java
+++ src/main/java/guru/springframework/model/Difficulty.java
@@ -2,7 +2,6 @@ package guru.springframework.model;
 
 import javax.persistence.Entity;
 
-@Entity
 public enum Difficulty {
     EASY, MODERATE, HARD
 }
--- src/main/java/guru/springframework/model/Recipe.java
+++ src/main/java/guru/springframework/model/Recipe.java
@@ -21,6 +21,15 @@ public class Recipe {
     @Enumerated(value = EnumType.STRING)
     private Difficulty difficulty;
 
+//    @ManyToMany
+//    private Set<Category> categories;
+
+    @ManyToMany
+    @JoinTable(name = "recipe_category",
+            joinColumns = @JoinColumn(name = "recipe_id"),
+            inverseJoinColumns = @JoinColumn(name = "category_id"))
+    private Set<Category> categories;
+
     @Lob
     private Byte[] image;
 
@@ -124,5 +133,13 @@ public class Recipe {
 
     public void setIngredients(Set<Ingredient> ingredients) {
         this.ingredients = ingredients;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
     }
 }
