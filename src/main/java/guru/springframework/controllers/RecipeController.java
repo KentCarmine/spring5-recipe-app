d0165f0dab6ab696df7c700ae70da6b16463c7d4
Kent Carmine <5168853+KentCarmine@users.noreply.github.com>
2019-09-19 16:32:39 -0500

    Added Recipe field validation

--- src/main/java/guru/springframework/controllers/RecipeController.java
+++ src/main/java/guru/springframework/controllers/RecipeController.java
@@ -8,15 +8,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
+
+import javax.validation.Valid;
 
 @Slf4j
 @Controller
 public class RecipeController {
 
-    private final RecipeService recipeService;
+    private static final String RECIPE_RECIPEFORM_URL = "recipe/recipeform";
 
+    private final RecipeService recipeService;
 
     @Autowired
     public RecipeController(RecipeService recipeService) {
@@ -34,21 +38,27 @@ public class RecipeController {
     public String newRecipe(Model model) {
         model.addAttribute("recipe", new RecipeCommand());
 
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
 
     @GetMapping("recipe/{id}/update")
     public String updateRecipe(@PathVariable String id, Model model) {
         model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
 
-        return "recipe/recipeform";
+        return RECIPE_RECIPEFORM_URL;
     }
 
-    @PostMapping
-    @RequestMapping("recipe")
-    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
-        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+    @PostMapping("recipe")
+    public String saveOrUpdate(@Valid @ModelAttribute("recipe") RecipeCommand recipeCommand, BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            bindingResult.getAllErrors().forEach(objectError -> {
+                log.debug(objectError.toString());
+            });
 
+            return RECIPE_RECIPEFORM_URL;
+        }
+
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
         return "redirect:/recipe/"  + savedCommand.getId() + "/show";
     }
 
@@ -72,18 +82,4 @@ public class RecipeController {
 
         return modelAndView;
     }
-
-//    @ResponseStatus(HttpStatus.BAD_REQUEST)
-//    @ExceptionHandler(NumberFormatException.class)
-//    public ModelAndView handleStringId(Exception e) {
-//        log.error("Handling String ID error");
-//        log.error(e.getMessage());
-//
-//        ModelAndView modelAndView = new ModelAndView();
-//        modelAndView.setViewName("400error");
-//        modelAndView.addObject("exception", e);
-//
-//        return modelAndView;
-//    }
-
 }
